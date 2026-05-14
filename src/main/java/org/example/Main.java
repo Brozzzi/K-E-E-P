b63532fed53163de8aee3e897adf51964ad75577
kai <kb@softwarepioniere.de>
2026-05-14 20:04:12 +0200

    Main loop erweitert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.Scanner;
+
+import de.gurkenlabs.input4j.InputDevice;
+import de.gurkenlabs.input4j.InputDevicePlugin;
+import de.gurkenlabs.input4j.InputDevices;
 
 public class Main {
     static void main() {
         // Setup
+        InputDevice selectedDevice;
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
 
@@ -26,17 +32,41 @@ public class Main {
 
             switch (eingabe) {
                 case "1":
+
+                    try (var inputDevices = InputDevices.init()) {
+                        while (!inputDevices.getAll().isEmpty()){
+                            var number = 1;
+                            for (var inputDevice : inputDevices.getAll()){
+                                inputDevice.poll();
+                                System.out.println(number + ". " + inputDevice.getProductName() + ":" + inputDevice.getComponents());
+                                number++;
+                            }
+                        }
+
+//                        if (inputDevice == null) {
+//                            System.out.println("Kein Gerät gefunden");
+//                            return;
+//                        var inputDevice = inputDevices.getAll().stream().findFirst().orElse(null);
+//
+//                        }
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+
+                    }
+
                     System.out.println("Funktion noch in Arbeit. Drücke Enter um fortzufahren...");
-//                    scanner.nextLine();
-
-
-
+                    scanner.nextLine();
                     break;
+
+
                 case "2":
                     System.out.println("Funktion noch in Arbeit. Drücke Enter um fortzufahren...");
                     scanner.nextLine();
 
                     break;
+
+
                 case "3":
                     System.out.println("Programm wird beendet. Drücke Enter um fortzufahren...");
                     scanner.nextLine();
